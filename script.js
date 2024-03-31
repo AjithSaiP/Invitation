4cf3202cc772bc6c036de3ed96ddefe6e62a7417
AjithSaiP <ajithsai.asp@kgpian.iitkgp.ac.in>
2024-03-31 18:31:39 +0530

    test

--- script.js
+++ script.js
@@ -1,8 +1,22 @@
 const button = document.getElementById("submit")
 const wrapper = document.getElementById("container1")
 const new_wrapper = document.getElementById("new_wrapper")
-
+const input = document.getElementById("input")
 button.addEventListener("click",()=>{
-    wrapper.style.display = "none"
-    new_wrapper.style.display = "block"
+    if(input.value.length!=0){
+        let flag = 0
+        for(let i = 0; i < input.value.length;i++){
+            if(input.value[i] == '0'|| input.value[i] =='1')
+                continue;
+            else{
+                flag = 1;
+                break;
+            }
+        }
+        if (flag == 0){
+        // if(input.value)
+        wrapper.style.display = "none"
+        new_wrapper.style.display = "block"
+    }
+}
 })
